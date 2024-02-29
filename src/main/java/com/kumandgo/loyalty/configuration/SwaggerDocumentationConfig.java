a714a7029d2e8a80553b874ba7a318b829460b6e
Brandon Kiesling <brandon.kiesling@kumandgo.com>
2024-02-29 15:12:23 -0600

    Partially fixed swagger page

--- src/main/java/com/kumandgo/loyalty/configuration/SwaggerDocumentationConfig.java
+++ src/main/java/com/kumandgo/loyalty/configuration/SwaggerDocumentationConfig.java
@@ -1,35 +1,18 @@
 package com.kumandgo.loyalty.configuration;
 
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Bean;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@EnableSwagger2
-public class SwaggerDocumentationConfig {
-
-    ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("Spring Data Jpa Snowflake Service API")
-                .description("Spring Data Jpa Snowflake Service API")
-                .license("")
-                .termsOfServiceUrl("")
-                .version("1.0.0")
-                .contact(new Contact("", "", ""))
-                .build();
-    }
+public class SwaggerDocumentationConfig
+{
 
     @Bean
-    public Docket customImplementation() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.kumandgo.loyalty.api"))
-                .build()
-                .apiInfo(apiInfo());
-    }
+    public OpenAPI springShopOpenAPI()
+    {
+        return new OpenAPI().info(new Info().title("SpringShop API").description("Spring shop sample application").version("v0.0.1").license(new License().name("Apache 2.0").url("http://springdoc.org"))).externalDocs(new ExternalDocumentation().description("SpringShop Wiki Documentation").url("https://springshop.wiki.github.org/docs"));
 
+    }
 }
